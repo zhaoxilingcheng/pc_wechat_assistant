b5870517efb5a157e283f19df6086adfde8fab2d
zhaoxilingcheng <30566370+zhaoxilingcheng@users.noreply.github.com>
2021-01-21 16:02:34 +0800

    Update launch.py

--- launch.py
+++ launch.py
@@ -5,7 +5,8 @@ back_o = '8bcee85d4b6f0085c0747b8bc8e8e2536f0085c07562'
 # 翻撤回更新后的字符
 back_n = '8bcee85d4b6f0085c0eb7b8bc8e8e2536f0085c07562'
 
-if __name__ == '__main__':
+
+def run():
     try:
         key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Tencent\\WeChat")
         value = winreg.QueryValueEx(key, "InstallPath")[0]
@@ -21,11 +22,21 @@ if __name__ == '__main__':
     with open(file_path, "rb") as f:
         hex_str = f.read().hex()
         # 防撤回替换
-        back_str = hex_str.replace(back_n,
-                                   back_o)
+        back_str = hex_str.replace(back_o,
+                                   back_n)
         # 双开替换
         file_data = back_str.replace('e86b00000084c07456566a00', 'e86b00000084c0eb56566a00')
 
     # 写入文件
     with open(file_path, "wb") as f:
         f.write(bytes.fromhex(file_data))
+
+
+if __name__ == '__main__':
+    try:
+        run()
+        print("执行成功")
+    except:
+        print("执行失败, 请检查是否未关闭微信程序!")
+
+    input("按任何按键退出!")
